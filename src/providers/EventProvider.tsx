85afbb5c1b0060f4c6837bb2f7b6d897ba8ae4e2
tolunayozdemir <tolunay.ozdemir.56@gmail.com>
2023-07-13 17:23:32 +0300

    AbortController added to event fetch

--- src/providers/EventProvider.tsx
+++ src/providers/EventProvider.tsx
@@ -1,26 +1,25 @@
-import React, {
-  FunctionComponent,
-  PropsWithChildren,
-  useEffect,
-  useState,
-} from "react";
+import React, { FunctionComponent, PropsWithChildren, useEffect, useState } from "react";
 import EventContext from "../contexts/EventContext";
 import { Event } from "../types";
 
-const EventProvider: FunctionComponent<PropsWithChildren> = ({
-  children,
-}) => {
+const EventProvider: FunctionComponent<PropsWithChildren> = ({ children }) => {
   const [events, setEvents] = useState<Event[]>([]);
 
-  const fetchEvents = async () => {
-    const result = await fetch("https://nesine-case-study.onrender.com/bets");
-    const events = await result.json();
-
-    setEvents(events);
-  };
-
   useEffect(() => {
+    const controller = new AbortController();
+
+    const fetchEvents = async () => {
+      const result = await fetch("https://nesine-case-study.onrender.com/bets", {
+        signal: controller.signal
+      });
+      const events = await result.json();
+
+      setEvents(events);
+    };
+
     fetchEvents();
+
+    return () => controller.abort();
   }, []);
 
   return <EventContext.Provider value={events}>{children}</EventContext.Provider>;
