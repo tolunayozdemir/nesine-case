23251031d0a42c40d0969d5f4cf776484b02b53c
tolunayozdemir <tolunay.ozdemir.56@gmail.com>
2023-07-13 22:07:38 +0300

    bet table header

--- src/components/BetTable.tsx
+++ src/components/BetTable.tsx
@@ -38,6 +38,29 @@ const BetTable = () => {
 
   return (
     <table className="bet-table">
+      <thead>
+        <tr>
+          <th>Event Count: {events.length}</th>
+          <th>Yorumlar</th>
+          <th></th>
+          <th>1</th>
+          <th>0</th>
+          <th>2</th>
+          <th>Alt</th>
+          <th>Üst</th>
+          <th>H1</th>
+          <th>1</th>
+          <th>X</th>
+          <th>2</th>
+          <th>H2</th>
+          <th>1-X</th>
+          <th>1-2</th>
+          <th>X-2</th>
+          <th>Var</th>
+          <th>Yok</th>
+          <th>+99</th>
+        </tr>
+      </thead>
       <tbody>
         {events.map((event) => {
           const ms1 = new Bet(event, "1", "0");
